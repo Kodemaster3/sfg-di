4088ff36c113068fff6143559348722f2e726b04
Sergey Lielik <vivson66@gmail.com>
2020-10-17 19:15:31 +0300

    exp to Primary Bean

--- src/main/java/guru/springFramework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springFramework/sfgdi/SfgDiApplication.java
@@ -13,15 +13,16 @@ public class SfgDiApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = SpringApplication.run(SfgDiApplication.class, args);
-		MyController myController = (MyController) applicationContext.getBean("myController");
-		String greet = myController.seyHello();
-		System.out.println(greet);
 		
-		System.out.println("------- Property");
+		System.out.println("-------- Primary Bean");
+		MyController myController = (MyController) applicationContext.getBean("myController");
+		System.out.println(myController.seyHello());
+		
+		System.out.println("-------- Property");
 		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) applicationContext.getBean("propertyInjectedController");
 		System.out.println(propertyInjectedController.getGreeting());
 		
-		System.out.println("--------- Setter");
+		System.out.println("-------- Setter");
 		SetterInjectionController setterInjectionController = (SetterInjectionController) applicationContext.getBean("setterInjectionController");
 		System.out.println(setterInjectionController.getGreeting());
 		
--- src/main/java/guru/springFramework/sfgdi/controllers/MyController.java
+++ src/main/java/guru/springFramework/sfgdi/controllers/MyController.java
@@ -1,12 +1,18 @@
 package guru.springFramework.sfgdi.controllers;
 
+import guru.springFramework.sfgdi.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
+    
+    private final GreetingService greetingService;
+    
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+    
     public String seyHello() {
-        System.out.println("Hello man!!");
-        
-        return "Hey dude";
+        return greetingService.sayGreeting();
     }
 }
